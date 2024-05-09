d3dc1e3ce89df142c396af8f70a6d1fe48c8d0ae
My name <ekkomain93@gmail.com>
2024-05-09 15:36:15 +0100

    adding NavBar component & other directories

---
+++ client/src/components/ui/NavBar.jsx
@@ -0,0 +1,35 @@
+import menu_icon from '../../assets/icons/menu_icon.svg';
+import x from '../../assets/icons/x.svg';
+import { useState } from "react";
+
+
+export default  function NavBar(){
+
+    const [isHidden, setIsHidden] = useState(true);
+    const [isConnect, setIsConnect] = useState(false);
+    const handleMenu = () => {
+        setIsHidden(isHidden ? false:true);
+    }
+    return (
+        <div className="flex flex-col w-full">
+           <div className={isHidden ? "hidden":""}>SideBar component</div>
+           <div className="flex justify-between px-6 py-4 w-full bg-teal-600 shadow-lg">
+                <div className="flex  justify-between w-1/2">
+                    <button onClick={()=>handleMenu()} className="sm:hidden"><img src={isHidden ? menu_icon:x} alt="menu icon" /></button>
+                    <div className="text-white font-bold text-xl">Logo</div>
+                </div>
+                <div> 
+                    {isConnect ? (
+                    <button className="text-black hover:text-white font-bold py-1 px-2">LogOut</button>
+                    ) : (
+                    <button className="bg-white text-red-600 hover:text-white hover:bg-red-600 font-bold rounded py-1 px-2">LogIn</button>)
+                    }
+                </div>
+
+           </div>
+        </div>
+
+
+
+    );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      tahoma: ["Tahoma", "Verdana", "Segoe", "sans-serif"],
+    },
+    
   },
   plugins: [],
 }
