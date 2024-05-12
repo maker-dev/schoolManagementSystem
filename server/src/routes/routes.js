0b9612259d54a07a6bc2cbb823014018d2884cdb
maker-dev <anasaizen9@gmail.com>
2024-05-12 17:56:06 +0100

    ADD verifyKey middleware

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { adminLogin, createTeacher, createStudent } from "../controllers/AdminController.js";
 import verifyToken from "../middlewares/verifyToken.js";
 import verifyRole from "../middlewares/verifyRole.js";
+import verifyKey from "../middlewares/verifyKey.js";
 
 const routes = Router();
 
-routes.post("/adminLogin", adminLogin);
-routes.post("/createTeacher",verifyToken, verifyRole(["admin"]), createTeacher);
-routes.post("/createStudent", verifyToken, verifyRole(["admin"]), createStudent);
+routes.post("/adminLogin", verifyKey, adminLogin);
+routes.post("/createTeacher", verifyKey, verifyToken, verifyRole(["admin"]), createTeacher);
+routes.post("/createStudent", verifyKey, verifyToken, verifyRole(["admin"]), createStudent);
 
 
 export {routes};
